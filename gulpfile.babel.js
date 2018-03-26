33f85ae0ae1c06d4d20d7a460ce9795f0fc08ad9
István Szegedi <pisty2@gmail.com>
2018-03-26 08:45:29 +0200

    axios finalize

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,12 +1,14 @@
 import gulp from "gulp";
 import browserSync from "browser-sync";
 
+const bs = browserSync.create();
+
 gulp.task('watch', () => {
-    browserSync({
+    bs.init({
         server: ['src'],
         cors: true,
         port: 3000
     });
-    gulp.watch(['src/**/*.css'], browserSync.stream());
+    gulp.watch(['src/**/*.css'], browserSync.reload);
     gulp.watch(['src/**/*.html'], browserSync.reload);
 });
